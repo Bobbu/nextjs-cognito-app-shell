abd36f64e657a8f8a530c088783bdd5c35f93964
Rob Daly <macbobbu@mac.com>
2025-06-13 12:40:50 -0400

    Test and refine Admin Area

--- src/app/dashboard/admins/page.tsx
+++ src/app/dashboard/admins/page.tsx
@@ -1,3 +1,12 @@
+import { lusitana } from "@/ui/fonts";
+
 export default function AdminArea() {
-  return <p>Admin Area</p>;
+  return <main>
+            <div className="flex w-full items-center justify-between mb-4">
+              <h1 className={`${lusitana.className} text-2xl`}>Admin Area</h1>
+            </div>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+              <p>You only get here is you are a member of the Admins group.</p>
+            </div>
+         </main>;
 }
